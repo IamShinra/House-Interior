1369a4090568eca195a0fbe396f43d8ee5ce1ce8
Shinra Zen <pratham18koli@gmail.com>
2026-02-08 18:07:25 +0530

    updated the img section

--- script.js
+++ script.js
@@ -21,7 +21,8 @@
 const discover_tl = gsap.timeline({
     scrollTrigger:{
         trigger: ".slide",
-        start: "20%",
+        start: "25%",
+        end: "150%",
         markers: true,
         scrub: 2,
         ease: "ease"
@@ -30,4 +31,36 @@ const discover_tl = gsap.timeline({
 
 discover_tl.to(".slide1",{
     y:220,
+})
+
+discover_tl.to(".slide2",{
+    y:220,
+})
+
+discover_tl.to(".slide3",{
+    y:220,
+})
+
+discover_tl.to(".slide4",{
+    y:220,
+})
+
+
+//image section animation
+
+gsap.to("#img_section2",{
+    clipPath: "circle(100% at 50% 50%)",
+    scrollTrigger: {
+        trigger: "image-section > .container",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 2,
+        pin: true,
+        onEnter: () => {
+            document.body.classList.add("dark-theme");
+        },
+        onLeaveBack : () => {
+            document.body.classList.remove("dark-theme")
+        }
+    }
 })
